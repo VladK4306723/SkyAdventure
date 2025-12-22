f74969c372e9ac1abcc15dda5f2340505352afa7
KREICHI VLADYSLAV <vladk4210@gmail.com>
2025-12-22 17:30:31 +0200

    Clouds fixed

--- Assets/Scripts/UI/CloudBackground.cs
+++ Assets/Scripts/UI/CloudBackground.cs
@@ -1,27 +1,35 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using Zenject;
 
-public class CloudBackground : MonoBehaviour
+public sealed class CloudSpawner : MonoBehaviour
 {
+    [Inject] private CameraBounds _bounds;
+
+    [Header("Initial Spawn")]
+    [SerializeField, Min(0)] private int _initialCloudCount = 4;
+
+
     [Header("References")]
-    [SerializeField] private RectTransform _container;
-    [SerializeField] private Image _cloudPrefab;
+    [SerializeField] private Transform _container;
+    [SerializeField] private SpriteRenderer _cloudPrefab;
 
     [Header("Spawn")]
     [SerializeField] private float _spawnInterval = 1.5f;
-    [SerializeField] private Vector2 _xRange = new Vector2(-180f, 180f);
     [SerializeField] private Vector2 _scaleRange = new Vector2(0.6f, 1.2f);
 
     [Header("Movement")]
-    [SerializeField] private Vector2 _speedRange = new Vector2(20f, 60f);
-    [SerializeField] private float _destroyOffset = 150f;
+    [SerializeField] private Vector2 _speedRange = new Vector2(0.5f, 1.5f);
+    [SerializeField] private float _destroyOffset = 1.5f;
 
     private float _timer;
     private readonly List<Cloud> _clouds = new();
 
-    private float TopY => _container.rect.height * 0.5f + _destroyOffset;
-    private float BottomY => -_container.rect.height * 0.5f - _destroyOffset;
+    private void Start()
+    {
+        SpawnInitialClouds();
+    }
+
 
     private void Update()
     {
@@ -36,22 +44,58 @@ public class CloudBackground : MonoBehaviour
         UpdateClouds();
     }
 
-    private void SpawnCloud()
+    private void SpawnInitialClouds()
     {
-        var cloudImage = Instantiate(_cloudPrefab, _container);
-        var rect = cloudImage.rectTransform;
+        for (int i = 0; i < _initialCloudCount; i++)
+        {
+            SpawnCloudAtRandomHeight();
+        }
+    }
+
+    private void SpawnCloudAtRandomHeight()
+    {
+        var cloud = Instantiate(_cloudPrefab, _container);
+        var t = cloud.transform;
 
         float scale = Random.Range(_scaleRange.x, _scaleRange.y);
-        rect.localScale = Vector3.one * scale;
+        t.localScale = Vector3.one * scale;
 
-        float x = Random.Range(_xRange.x, _xRange.y);
-        rect.anchoredPosition = new Vector2(x, TopY);
+        float x = Random.Range(_bounds.Left, _bounds.Right);
+        float y = Random.Range(
+            _bounds.Bottom - _destroyOffset,
+            _bounds.Top + _destroyOffset
+        );
+
+        t.position = new Vector3(x, y, 0f);
 
         float speed = Random.Range(_speedRange.x, _speedRange.y);
 
         _clouds.Add(new Cloud
         {
-            Rect = rect,
+            Transform = t,
+            Speed = speed
+        });
+    }
+
+
+    private void SpawnCloud()
+    {
+        var cloud = Instantiate(_cloudPrefab, _container);
+        var t = cloud.transform;
+
+        float scale = Random.Range(_scaleRange.x, _scaleRange.y);
+        t.localScale = Vector3.one * scale;
+
+        float x = Random.Range(_bounds.Left, _bounds.Right);
+        float y = _bounds.Top + _destroyOffset;
+
+        t.position = new Vector3(x, y, 0f);
+
+        float speed = Random.Range(_speedRange.x, _speedRange.y);
+
+        _clouds.Add(new Cloud
+        {
+            Transform = t,
             Speed = speed
         });
     }
@@ -61,13 +105,14 @@ public class CloudBackground : MonoBehaviour
         for (int i = _clouds.Count - 1; i >= 0; i--)
         {
             var cloud = _clouds[i];
-            var pos = cloud.Rect.anchoredPosition;
-            pos.y -= cloud.Speed * Time.deltaTime;
-            cloud.Rect.anchoredPosition = pos;
+            var pos = cloud.Transform.position;
 
-            if (pos.y < BottomY)
+            pos.y -= cloud.Speed * Time.deltaTime;
+            cloud.Transform.position = pos;
+
+            if (pos.y < _bounds.Bottom - _destroyOffset)
             {
-                Destroy(cloud.Rect.gameObject);
+                Destroy(cloud.Transform.gameObject);
                 _clouds.RemoveAt(i);
             }
         }
@@ -75,7 +120,7 @@ public class CloudBackground : MonoBehaviour
 
     private struct Cloud
     {
-        public RectTransform Rect;
+        public Transform Transform;
         public float Speed;
     }
 }
